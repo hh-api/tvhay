09f16850c3c87a9daf436596eac1fb0b65fba283
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-24 21:54:48 +0700

    Create BXH.php

---
+++ includes/BXH.php
@@ -0,0 +1,17 @@
+<div class="block" id="chart">
+
+<div class="blocktitle"><div class="title">Lịch Chiếu Phim</div></div>
+<table width='100%'><tr>
+  <td width='50%'><a href='/index.php?type=T2'><button style='width:100%; color:white; padding: 10px 5px; background: #c31432;background: -webkit-linear-gradient(to right, #c31432, #240b36);background: linear-gradient(to right, #c31432, #240b36);'><b>T2</b></button></a></td>
+  <td width='50%'><a href='/index.php?type=T3'><button style='width:100%; color:white; padding: 10px 5px; background: #009fff; background: -webkit-linear-gradient(to right, #009fff, #ec2f4b);background: linear-gradient(to right, #009fff, #ec2f4b);'><b>T3</b></button></a></td>
+</tr><tr>  
+  <td width='50%'><a href='/index.php?type=T4'><button style='width:100%; color:white; padding: 10px 5px; background: #cc5333; background: -webkit-linear-gradient(to right, #cc5333, #23074d); background: linear-gradient(to right, #cc5333, #23074d);'><b>T4</b></button></a></td>
+  <td width='50%'><a href='/index.php?type=T5'><button style='width:100%; color:white; padding: 10px 5px; background: #000000; background: -webkit-linear-gradient(to left, #000000, #0f9b0f); background: linear-gradient(to left, #000000, #0f9b0f);'><b>T5</b></button></a></td>
+</tr><tr>  
+  <td width='50%'><a href='/index.php?type=T6'><button style='width:100%; color:white; padding: 10px 5px; background: #00bf8f; background: -webkit-linear-gradient(to right, #00bf8f, #001510); background: linear-gradient(to right, #00bf8f, #001510);'><b>T6</b></button></a></td>
+  <td width='50%'><a href='/index.php?type=T7'><button style='width:100%; color:white; padding: 10px 5px;background: #eb5757; background: -webkit-linear-gradient(to right, #eb5757, #000000); background: linear-gradient(to right, #eb5757, #000000);'><b>T7</b></button></a></td>
+</tr><tr>  
+  <td colspan="2" width='100%'><a href='/index.php?type=CN'><button style='width:100%; color:white; padding: 10px 5px;background: #03001e; background: -webkit-linear-gradient(to left, #03001e, #7303c0, #ec38bc, #fdeff9); background: linear-gradient(to left, #03001e, #7303c0, #ec38bc, #fdeff9);'><b>Chủ Nhật</b></button></a></td>
+</tr></table>
+
+            </div>
