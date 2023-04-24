1dc1184a48c89489d4b5eaca496137b4fe24c7ed
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-24 21:53:21 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+<div class="block2">
+<div class="container">
+<div class="colweb">
+<a href="https://TVHAY.ORG" title="TVHAY.ORG Xem Phim Hoạt Hình Thuyết Minh, Lồng Tiếng" target="_blank"><font color="red">TVHAY.ORG</font></a> Với mong muốn mang đến dịch vụ phim hoạt hình thuyết minh, lồng tiếng nhanh nhất, tốt nhất. </div>
+<div class="colright">
+<div class="copyright">
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
