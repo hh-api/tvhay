a6a90a140c29ef1bb16cbb5224d2f4a6db06311c
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-24 21:54:30 +0700

    Create MENU.php

---
+++ includes/MENU.php
@@ -0,0 +1,103 @@
+<div class="block">
+                <div class="blocktitle"><span class="title">Chuyên Mục</span>
+                </div>
+                <div class="blockbody nocorner catitems">
+                    <div class="subtitle">Thể Loại Phim</div>
+                    <ul><li id="menu-item-1" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-61">
+                            <h6><a href="/index.php?type=Cổ Trang">Cổ Trang</a></h6>
+                        </li>
+                        <li id="menu-item-2" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-72">
+                            <h6><a href="/index.php?type=Viễn Tưởng">Viễn Tưởng</a></h6>
+                        </li>
+                        <li id="menu-item-3" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-63">
+                            <h6><a href="/index.php?type=Trùng Sinh">Trùng Sinh</a></h6>
+                        </li>
+                        <li id="menu-item-4" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-236">
+                            <h6><a href="/index.php?type=Luyện Cấp">Luyện Cấp</a></h6>
+                        </li>
+                        <li id="menu-item-5" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-73">
+                            <h6><a href="/index.php?type=Xuyên Không">Xuyên Không</a></h6>
+                        </li>
+                        <li id="menu-item-6" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-69">
+                            <h6><a href="/index.php?type=Tình Yêu">Tình Yêu</a></h6>
+                        </li>
+                        <li id="menu-item-7" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-67">
+                            <h6><a href="/index.php?type=Kinh Dị">Kinh Dị</a></h6>
+                        </li>
+                        <li id="menu-item-8" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-64">
+                            <h6><a href="/index.php?type=Hiện Đại">Hiện Đại</a></h6>
+                        </li>
+                        <li id="menu-item-62" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-62">
+                            <h6><a href="/index.php?type=Hệ Thống">Hệ Thống</a></h6>
+                        </li>
+                        <li id="menu-item-65" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-65">
+                            <h6><a href="/index.php?type=Tương Lai">Tương Lai</a></h6>
+                        </li>
+                        <li id="menu-item-68" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-68">
+                            <h6><a href="/index.php?type=Phiêu Lưu">Phiêu Lưu</a></h6>
+                        </li>
+                        <li id="menu-item-60" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-60">
+                            <h6><a href="/index.php?type=Chiến Tranh">Chiến Tranh</a></h6>
+                        </li>
+                        <li id="menu-item-70" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-70">
+                            <h6><a href="/index.php?type=Thần Thoại">Thần Thoại</a></h6>
+                        </li>
+                        <li id="menu-item-59" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-59">
+                            <h6><a href="/index.php?type=Âm Nhạc">Âm Nhạc</a></h6>
+                        </li>
+                        <li id="menu-item-66" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-66">
+                            <h6><a href="/index.php?type=Khoa Học">Khoa Học</a></h6>
+                        </li>
+                        <li id="menu-item-66" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-66">
+                            <h6><a href="/index.php?type=Con Gái">Con Gái</a></h6>
+                        </li>
+                    </ul>
+                </div>
+                <div class="blockbody nocorner catitems">
+                    <div class="subtitle">Nam Chiếu</div>
+                    <ul>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2023">2023</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2022">2022</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2021">2021</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2020">2020</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2019">2019</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2018">2018</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2017">2017</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2016">2016</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2015">2015</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2014">2014</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2013">2013</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2012">2012</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2011">2011</a></h6>
+                        </li>
+                        <li id="menu-item-15" class="menu-item menu-item-type-taxonomy">
+                            <h6><a href="/index.php?type=2010">2010</a></h6>
+                        </li>
+                    </ul>
+                </div>
+            </div>
