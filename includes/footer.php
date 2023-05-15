f462c72c3d34d61bc6b969f393fafa63a6882081
hh-api <131185043+hh-api@users.noreply.github.com>
2023-05-15 23:19:02 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <div class="block2">
 <div class="container">
 <div class="colweb">
-<a href="https://TVHAY.ORG" title="TVHAY.ORG Xem Phim Hoạt Hình Thuyết Minh, Lồng Tiếng" target="_blank"><font color="red">TVHAY.ORG</font></a> Với mong muốn mang đến dịch vụ phim hoạt hình thuyết minh, lồng tiếng nhanh nhất, tốt nhất. </div>
+<a href="https://oTVHAY.ORG" title="TVHAY.ORG Xem Phim Hoạt Hình Thuyết Minh, Lồng Tiếng" target="_blank"><font color="red">TVHAY.ORG</font></a> Với mong muốn mang đến dịch vụ phim hoạt hình thuyết minh, lồng tiếng nhanh nhất, tốt nhất. </div>
 <div class="colright">
 <div class="copyright">
 </div>
